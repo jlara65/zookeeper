3d4ff492c609b65a809df3d8fef93c53d6c6761c
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-19 01:04:31 -0500

    add more filter and set port listen

--- server.js
+++ server.js
@@ -55,3 +55,18 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    res.json(result);
+});
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
