f79a3b079218ef3130b58d0c19d2323ef15c6d0a
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-19 01:09:59 -0500

    quick fix in the correct order in server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
-const { animals } = require('./data/animals.json');
-
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -24,12 +20,6 @@ function filterByQuery(query, animalsArray) {
       // Loop through each trait in the personalityTraits array:
       personalityTraitsArray.forEach(trait => {
         // Check the trait against each animal in the filteredResults array.
-        // Remember, it is initially a copy of the animalsArray,
-        // but here we're updating it for each trait in the .forEach() loop.
-        // For each trait being targeted by the filter, the filteredResults
-        // array will then contain only the entries that contain the trait,
-        // so at the end we'll have an array of animals that have every one 
-        // of the traits when the .forEach() loop is finished.
         filteredResults = filteredResults.filter(
           animal => animal.personalityTraits.indexOf(trait) !== -1
         );
@@ -46,6 +36,11 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+}
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -66,7 +61,6 @@ app.get('/api/animals/:id', (req, res) => {
     res.json(result);
 });
 
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
